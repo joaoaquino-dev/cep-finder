ba781b5f08ec65f5b7b2e470ea697e236394f180
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-04-17 17:11:10 -0300

    feat: add address display, error handling and input validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import { useState } from "react";
 
+import type { Address } from "./types";
+
 const App = () => {
   const [cep, setCep] = useState("");
-  const [address, setAddress] = useState(null);
+  const [address, setAddress] = useState<Address | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   async function handleSearch() {
+    setError("");
+    const cleanCep = cep.replace("-", "");
+
+    if (cleanCep.length < 1) {
+      setError("Digite um CEP");
+      return;
+    }
+
+    if (cleanCep.length !== 8) {
+      setError("CEP deve ter 8 dígitos");
+      return;
+    }
     setLoading(true);
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await response.json();
 
-    console.log(data);
+    try {
+      const response = await fetch(
+        `https://viacep.com.br/ws/${cleanCep}/json/`,
+      );
+      const data = await response.json();
 
-    setLoading(false);
+      if (data.erro) {
+        setError("CEP não encontrado");
+        setLoading(false);
+        return;
+      }
+
+      setAddress(data);
+
+      setLoading(false);
+    } catch (e) {
+      setError("Algo deu errado, tente novamente.");
+      setLoading(false);
+      return;
+    }
   }
 
   return (
@@ -27,6 +56,17 @@ const App = () => {
       />
       <button onClick={handleSearch}>Buscar</button>
       {loading && <p>Carregando...</p>}
+      {error && <p>{error}</p>}
+      {address && (
+        <div>
+          <ul>
+            <li>{address.logradouro}</li>
+            <li>{address.bairro}</li>
+            <li>{address.localidade}</li>
+            <li>{address.uf}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Address = {
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  estado: string;
+};
