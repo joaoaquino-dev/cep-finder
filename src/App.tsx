74e959cedf05350fda154fdeea2dd9ad47e181f4
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-04-27 13:34:04 -0300

    feat: add Tailwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -46,24 +46,39 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>CEP Finder</h1>
+    <div className="flex justify-center items-center flex-col bg-gray-950 min-h-screen">
+      <h1 className="font-bold text-4xl mb-6 text-green-500">CEP Finder</h1>
       <input
         type="text"
         placeholder="Digite o CEP"
         value={cep}
         onChange={(e) => setCep(e.target.value)}
+        className="flex bg-black text-white rounded-md p-2 mt-4 w-80 outline-none border border-gray-700 placeholder:text-gray-500"
       />
-      <button onClick={handleSearch}>Buscar</button>
-      {loading && <p>Carregando...</p>}
-      {error && <p>{error}</p>}
+      <button
+        onClick={handleSearch}
+        className="cursor-pointer bg-yellow-300 text-gray-900 rounded-full px-4 py-2 mt-4 w-80"
+      >
+        Buscar
+      </button>
+      {loading && <p className="text-yellow-300 mt-4">Carregando...</p>}
+      {error && <p className="text-red-400 mt-4">{error}</p>}
       {address && (
-        <div>
-          <ul>
-            <li>{address.logradouro}</li>
-            <li>{address.bairro}</li>
-            <li>{address.localidade}</li>
-            <li>{address.uf}</li>
+        <div className="bg-gray-800 rounded-lg p-4 mt-4 w-80">
+          <ul className="text-white space-y-2">
+            <li>
+              <span className="text-green-400">Rua:</span> {address.logradouro}
+            </li>
+            <li>
+              <span className="text-green-400">Bairro:</span> {address.bairro}
+            </li>
+            <li>
+              <span className="text-green-400">Cidade:</span>{" "}
+              {address.localidade}
+            </li>
+            <li>
+              <span className="text-green-400">UF:</span> {address.uf}
+            </li>
           </ul>
         </div>
       )}
